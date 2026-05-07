de8122b661bf5149985478f1a97ff729c6ac054e
Zaki.Khan <zakiulla.khan@outlook.com>
2026-05-07 20:43:51 +0300

    testing more

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ x = 0
 
 while x > 10:
     y = input("Enter the number you want:")
+    y = int(y)
+    if y > x:
+        print("y is greater than x")
+    else:
+        print("nothing is greater than x")
+
+    x = x + 1
+    
